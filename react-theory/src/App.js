62d9cdd18db98b75dc9a1b16af5571ae209e4866
Aleksandr-Glebik <glebikaa@mail.ru>
2022-07-18 21:37:17 +0300

    feat: add event listener for button app and car components two methods

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import './App.css';
 import Car from './Car/Car';
 
 
@@ -14,15 +13,7 @@ class App extends Component {
     pageTitle: 'React components'
   }
 
-  // changeTitleHandler() {
-  // }
-  // changeTitleHandler: function () {
-  // }
-  changeTitleHandler = () => {
-    const oldTitle = this.state.pageTitle
-    const newTitle = oldTitle + ' (changed)'
-    // console.log('click!')
-
+  changeTitleHandler = (newTitle) => {
     this.setState({
       pageTitle: newTitle
     })
@@ -45,33 +36,26 @@ class App extends Component {
           <h1>{this.state.pageTitle}</h1>
         </div>
 
-        <button onClick={this.changeTitleHandler}>Change title</button>
+        <button onClick={this.changeTitleHandler.bind(this, 'changed!')}>Change title</button>
 
-        <Car name={cars[0].name} year={cars[0].year} />
-        <Car name={cars[1].name} year={cars[1].year} />
-        <Car name={cars[2].name} year={cars[2].year} />
+        <Car
+         name={cars[0].name}
+         year={cars[0].year}
+         onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}
+        />
+        <Car
+         name={cars[1].name}
+         year={cars[1].year}
+         onChangeTitle={() => this.changeTitleHandler(cars[1].name)}
+        />
+        <Car
+         name={cars[2].name}
+         year={cars[2].year}
+         onChangeTitle={() => this.changeTitleHandler(cars[2].name)}
+        />
       </div>
     )
   }
 }
-
-// const divStyle = {
-//   textAlign: 'center'
-// }
-
-// function App() {
-//   return (
-//     <div style={divStyle}>
-//       <div>
-//         <h1>Hello world!!!</h1>
-//       </div>
-//       <p>I am Frontend developer</p>
-
-//       <Car name={'Citroen C4'} year={'2005'} />
-//       <Car name={'Toyota Camry'} year={'2020'} />
-//       <Car name={'BMW M5'} year={'2022'} />
-//     </div>
-//   )
-// }
 
 export default App;
