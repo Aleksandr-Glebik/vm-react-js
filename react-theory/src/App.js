ac4b26a77d4e5cb27be2a7446e4d19b52499596f
Aleksandr-Glebik <glebikaa@mail.ru>
2022-07-18 17:04:20 +0300

    feat: add inline-styles

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -1,13 +1,17 @@
- import React from 'react';
-import './App.css';
+import React from 'react';
+// import './App.css';
+
+const divStyle = {
+  textAlign: 'center'
+}
 
 function App() {
   return (
-    <div>
-      <div className="App">
+    <div style={divStyle}>
+      <div>
         <h1>Hello world!!!</h1>
       </div>
-      <p className="App">I AM Frontend developer</p>
+      <p style={{color: 'red', fontSize: '40px'}}>I am Frontend developer</p>
     </div>
   )
 }
