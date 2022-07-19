5d35079d72c4625fa9257f15863b152b71ee8086
Aleksandr-Glebik <glebikaa@mail.ru>
2022-07-19 13:54:55 +0300

    refactor: component Car from func comp to class component

--- react-theory/src/Car/Car.js
+++ react-theory/src/Car/Car.js
@@ -1,29 +1,59 @@
 import React, { Children } from 'react';
-import style from './Car.css'
+import './Car.css'
+class Car extends React.Component {
+   render() {
+      const inputClasses = ['input']
 
-export default (props) => {
- const inputClasses = ['input']
+      if (this.props.name !== '') {
+         inputClasses.push('green')
+      } else {
+         inputClasses.push('red')
+      }
 
- if (props.name !== '') {
-    inputClasses.push('green')
- } else {
-    inputClasses.push('red')
- }
+      if (this.props.name.length > 10) {
+         inputClasses.push('bold')
+      }
 
- if (props.name.length > 10) {
-    inputClasses.push('bold')
- }
-
- return (
-  <div className='Car'>
-    <h3>Car name: {props.name}</h3>
-    <p>Year: <strong>{props.year}</strong></p>
-    <input
-      onChange={props.onChangeName}
-      value={props.name}
-      className={inputClasses.join(' ')}
-    />
-    <button onClick={props.onDelete}>Delete</button>
-  </div>
- )
+      return (
+       <div className='Car'>
+         <h3>Car name: {this.props.name}</h3>
+         <p>Year: <strong>{this.props.year}</strong></p>
+         <input
+           onChange={this.props.onChangeName}
+           value={this.props.name}
+           className={inputClasses.join(' ')}
+         />
+         <button onClick={this.props.onDelete}>Delete</button>
+       </div>
+      )
+   }
 }
+
+export default Car
+
+// export default (props) => {
+//  const inputClasses = ['input']
+
+//  if (props.name !== '') {
+//     inputClasses.push('green')
+//  } else {
+//     inputClasses.push('red')
+//  }
+
+//  if (props.name.length > 10) {
+//     inputClasses.push('bold')
+//  }
+
+//  return (
+//   <div className='Car'>
+//     <h3>Car name: {props.name}</h3>
+//     <p>Year: <strong>{props.year}</strong></p>
+//     <input
+//       onChange={props.onChangeName}
+//       value={props.name}
+//       className={inputClasses.join(' ')}
+//     />
+//     <button onClick={props.onDelete}>Delete</button>
+//   </div>
+//  )
+// }
