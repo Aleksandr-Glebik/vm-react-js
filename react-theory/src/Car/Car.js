bab4f1158277fca699a0270d14915e2b8cc4129c
Aleksandr-Glebik <glebikaa@mail.ru>
2022-07-19 17:06:31 +0300

    feat: add hoc withClass

--- react-theory/src/Car/Car.js
+++ react-theory/src/Car/Car.js
@@ -1,70 +1,35 @@
 import React, { Children } from 'react';
-import './Car.css'
+import classes from './Car.module.css'
+import withClass from '../hoc/withClass';
 class Car extends React.Component {
 
-   // componentWillReceiveProps(nextProps) {
-   //    console.log('Car componentWillReceiveProps', nextProps);
-   // }
-
-   shouldComponentUpdate(nextProps, nextState) {
-      console.log('Car shouldComponentUpdate', nextProps, nextState);
-      return nextProps.name.trim() !== this.props.name.trim()
-   }
-
-   // componentWillUpdate(nextProps, nextState) {
-   //    console.log('Car componentWillUpdate', nextProps, nextState);
-   // }
-
-   // static getDerivedStateFromProps(nextProps, prevState) {
-   //    console.log('Car getDerivedStateFromProps', nextProps, prevState);
-
-   //    return prevState
-   // }
-
-   componentDidUpdate() {
-      console.log('Car componentDidUpdate');
-   }
-
-   // getSnapshotBeforeUpdate() {
-   //    console.log('Car getSnapshotBeforeUpdate');
-   // }
-
-   componentWillUnmount() {
-      console.log('Car componentWillUnmount');
-   }
-
    render() {
-      console.log('render Car');
-
-      // if (Math.random() > 0.99) {
-      //    throw new Error('Car random failed')
-      // }
-
-      const inputClasses = ['input']
+      const inputClasses = [classes.input]
 
       if (this.props.name !== '') {
-         inputClasses.push('green')
+         inputClasses.push(classes.green)
       } else {
-         inputClasses.push('red')
+         inputClasses.push(classes.red)
       }
 
       if (this.props.name.length > 10) {
-         inputClasses.push('bold')
+         inputClasses.push(classes.bold)
       }
 
       return (
-       <div className='Car'>
+       <React.Fragment>
          <h3>Car name: {this.props.name}</h3>
          <p>Year: <strong>{this.props.year}</strong></p>
          <input
+           type={'text'}
            onChange={this.props.onChangeName}
            value={this.props.name}
            className={inputClasses.join(' ')}
          />
          <button onClick={this.props.onDelete}>Delete</button>
-       </div>
+       </React.Fragment>
       )
    }
 }
 
-export default Car
+export default withClass(Car, classes.Car)
