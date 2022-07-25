c133c159dd8a08929489c96e377e1511c2e6b681
Aleksandr-Glebik <glebikaa@mail.ru>
2022-07-25 16:44:59 +0300

    feat: add new project, start learn redux

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -1,56 +1,28 @@
 import React, { Component } from 'react'
 import './App.scss'
-import About from './About/About'
-import Cars from './Cars/Cars'
-import { Routes, Route, NavLink } from 'react-router-dom'
-import CarDetail from './CarDetail/CarDetail'
 
 class App extends Component {
-
   state = {
-    isLoggedIn: false
+    counter: 0
+  }
+
+  updateCounter(value) {
+    this.setState({
+      counter: this.state.counter + value
+    })
   }
 
   render() {
-
     return (
-      <div>
-        <nav className="nav">
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about">About</NavLink>
-            </li>
-            <li>
-              <NavLink to="/cars">Cars</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <hr />
-        <div style={{textAlign: 'center'}}>
-          <h3>Is logged in {this.state.isLoggedIn ? 'true' : 'false'}</h3>
-          <button
-            onClick={() => this.setState({
-              isLoggedIn: !this.state.isLoggedIn
-            })}>Login</button>
-        </div>
-
+      <div className={'App'}>
+        <h1>Счетчик <strong>{this.state.counter}</strong></h1>
 
         <hr/>
-        <Routes>
-          <Route path="/" element={<h1 style={{textAlign: 'center'}}>Home page</h1>}/>
 
-          { this.state.isLoggedIn ? <Route path="/about" element={<About />}/> : null }
-
-          <Route path="/cars" element={<Cars />}/>
-          <Route path="/cars">
-            {/* <Route path=":name" element={<h1>Carrrrr</h1>} /> */}
-            <Route path=":name" element={<CarDetail />} />
-          </Route>
-          {/* <Route path="*" element={<h1 style={{textAlign: 'center', color: 'red'}}>404 not found</h1>} /> */}
-        </Routes>
+        <div className="Actions">
+          <button onClick={() => this.updateCounter(1)}>Добавить 1</button>
+          <button onClick={() => this.updateCounter(-1)}>Вычесть 1</button>
+        </div>
       </div>
     )
   }
