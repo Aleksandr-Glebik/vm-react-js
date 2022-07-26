3d59f54f140301c69884f7c602e31ef3344ad758
Aleksandr-Glebik <glebikaa@mail.ru>
2022-07-26 11:40:13 +0300

    feat: add connect, mapStateToProps, mapDispatchToProps - link and change global state

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -1,31 +1,43 @@
 import React, { Component } from 'react'
 import './App.scss'
+import {connect} from 'react-redux'
 
 class App extends Component {
-  state = {
-    counter: 0
-  }
 
-  updateCounter(value) {
-    this.setState({
-      counter: this.state.counter + value
-    })
-  }
+  // updateCounter(value) {
+  //   // this.setState({
+  //   //   counter: this.props.counter + value
+  //   // })
+  // }
 
   render() {
+    // console.log('this.props', this.props);
     return (
       <div className={'App'}>
-        <h1>Счетчик <strong>{this.state.counter}</strong></h1>
+        <h1>Счетчик <strong>{this.props.counter}</strong></h1>
 
         <hr/>
 
         <div className="Actions">
-          <button onClick={() => this.updateCounter(1)}>Добавить 1</button>
-          <button onClick={() => this.updateCounter(-1)}>Вычесть 1</button>
+          <button onClick={this.props.onAdd}>Добавить 1</button>
+          <button onClick={this.props.onSub}>Вычесть 1</button>
         </div>
       </div>
     )
   }
 }
 
-export default App
+function mapStateToProps(state) {
+  return {
+    counter: state.counter
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onAdd: () => dispatch({type: 'ADD'}),
+    onSub: () => dispatch({type: 'SUB'}),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
